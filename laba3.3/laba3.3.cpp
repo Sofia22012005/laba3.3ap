d8e01295c12466fc5192d50974c748998ed07eb2
Sofia <kulchitskasofia@gmail.com>
2022-10-09 18:10:47 +0300

    laba3.3

---
+++ laba3.3/laba3.3.cpp
@@ -0,0 +1,36 @@
+﻿// Lab_03_3.cpp
+// < Кульчицька Софія Олександрівна >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 7
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний аргумент
+
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= R)
+		y = R;
+	else
+		if ( R < x && x <= 0)
+			y = R - sqrt(R * R - x * x);
+		else
+			if (0 < x && x <= R)
+				y = R - sqrt(R * R - x * x);
+			else
+				if (R < x && x <=6)
+					y = R + (-3-R)/(6-R)*(x-R);
+				else
+					y = -3*x+18;
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
